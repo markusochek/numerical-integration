f4230173456876697f68912fc56c2539cd85645f
Sagalaev Mark <sagalaev.mark@gmail.com>
2023-05-02 14:52:24 +0400

    completed

--- GaussQuadrature.js
+++ GaussQuadrature.js
@@ -15,27 +15,6 @@ export function gaussQuadrature(f, a, b) {
     }
     return sumIntegral;
 }
-function calculationCombinations(n) {
-    let couples = []
-    for (let i = 0; i < n; i++) {
-        couples.push(["0", i+1]);
-    }
-    let terms = []
-    for (let i = 0; i < Math.pow(2, n); i++) {
-        let binaryNumber = i.toString(2);
-        while (binaryNumber.length !== n) {
-            binaryNumber = "0" + String(binaryNumber);
-        }
-        let numberWithoutZeros = "";
-        for (let j = 0; j < binaryNumber.length; j++) {
-            if (couples[j][binaryNumber[j]] === "0") continue;
-            numberWithoutZeros += couples[j][binaryNumber[j]]
-        }
-        terms.push(numberWithoutZeros)
-    }
-    return terms;
-}
-
 
 function calculationIntegral(n, b, a) {
     let coefficients = []
